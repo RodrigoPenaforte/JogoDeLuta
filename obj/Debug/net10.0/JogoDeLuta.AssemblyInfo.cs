026e5ad1e3f4fa77c47e620ae06751ac79dfc13a
Rodrigo Penaforte <86313462+RodrigoPenaforte@users.noreply.github.com>
2026-03-18 09:44:00 -0300

    ajustando o md

--- obj/Debug/net10.0/JogoDeLuta.AssemblyInfo.cs
+++ obj/Debug/net10.0/JogoDeLuta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoDeLuta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+847a20d5578fd3938cf688b9eb33a63c1ef08d9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoDeLuta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoDeLuta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
